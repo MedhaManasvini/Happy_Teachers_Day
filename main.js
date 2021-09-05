10a1f9061c9398d66654a3314ded4b9ee256a57a
MedhaManasvini <79033962+MedhaManasvini@users.noreply.github.com>
2021-09-05 09:56:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+var x = document.getElementById("myAudio");
+
+function openComp() {
+    window.location = "computer.html";
+
+    x.play();
+    console.log("play");
+}
+
+function openMathematics() {
+    window.location = "maths.html";
+
+    x.play();
+    console.log("play");
+}
+
+function openScience() {
+    window.location = "science.html";
+
+    x.play();
+    console.log("play");
+}
+
+function openEnglish() {
+    window.location = "english.html";
+    x.play();
+}
+
+function openSSt() {
+    window.location = "sst.html";
+
+    x.play();
+    console.log("play");
+}
+
+function openHindi() {
+    window.location = "hindi.html";
+
+    x.play();
+    console.log("play");
+}
+
+function openSanskrit() {
+    window.location = "sanskrit.html";
+
+    x.play();
+    console.log("play");
+}
+
+function openCoding() {
+    window.location = "coding.html";
+
+    x.play();
+    console.log("play");
+}
+
+function openPiano() {
+    window.location = "piano.html";
+
+    x.play();
+    console.log("play");
+}
+
+function openDance1() {
+    window.location = "dance1.html";
+
+    x.play();
+    console.log("play");
+}
+
+function openDance2() {
+    window.location = "dance2.html";
+
+    x.play();
+    console.log("play");
+}
+
+function openSinging() {
+    window.location = "singing.html";
+
+    x.play();
+    console.log("play");
+}
+function openDrawing() {
+    window.location = "drawing.html";
+
+    x.play();
+    console.log("play");
+}
